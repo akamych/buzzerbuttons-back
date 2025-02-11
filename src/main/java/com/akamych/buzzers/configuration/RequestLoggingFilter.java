97638640631aacb1c607b0d7e37b1004aaf5f4fa
Alexey "akamych" Kalinin <akamych@gmail.com>
2025-02-11 10:43:46 -0300

    debug filter

---
+++ src/main/java/com/akamych/buzzers/configuration/RequestLoggingFilter.java
@@ -0,0 +1,31 @@
+package com.akamych.buzzers.configuration;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class RequestLoggingFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        logger.info("Request received: {} {}", request.getRemoteAddr(), request.getLocalName());
+        chain.doFilter(request, response);  // Proceed with the next filter or request handler
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization logic, if needed
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup logic, if needed
+    }
+}
